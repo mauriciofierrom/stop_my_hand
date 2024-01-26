d47824ccc32a0b269fe25e00e3be98ba2b21f4e3
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-01-26 09:51:41 -0500

    Setup CI

--- config/test.exs
+++ config/test.exs
@@ -9,8 +9,7 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :stop_my_hand, StopMyHand.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("POSTGRES_USER") || "mauricio",
   hostname: "localhost",
   database: "stop_my_hand_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
