a90c3ef262a684490bbc9b22b21f5a3a2a2d3d0a
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-03-25 01:11:22 -0500

    Add ExDoc
    To be able to generate documentation as html. Makes learning the
    corecomponents we use for now easier.

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule StopMyHand.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_doc, "~> 0.25.0", only: :dev}
     ]
   end
 
