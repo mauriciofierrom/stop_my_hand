a3212a27df0194b9237c4115d4bfb8dab456afbe
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-10-03 07:43:24 -0500

    Allow newer elixir versions in deps
    - Relax constraints
    - Make updates required by newer versions of deps

--- lib/stop_my_hand_web/gettext.ex
+++ lib/stop_my_hand_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule StopMyHandWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :stop_my_hand
+  use Gettext.Backend, otp_app: :stop_my_hand
 end
