2e2060d1ae1d64def734ff07d31797049dbb8e4a
Mauricio Fierro <mauriciofierrom@gmail.com>
2024-10-09 09:35:46 -0500

    Add fixture with invited and preload invitee
    - Add fixture with an argument to indicate what the invited user is
    - In both invite fixtures preload the invitee (for now only this)

--- test/support/fixtures/friendship_fixtures.ex
+++ test/support/fixtures/friendship_fixtures.ex
@@ -5,12 +5,20 @@ defmodule StopMyHand.FriendshipFixtures do
 
   alias StopMyHand.Accounts.{User}
   import StopMyHand.AccountsFixtures
+  alias StopMyHand.Repo
 
   def invite_fixture() do
     %User{id: invitee_id} = user_fixture()
     %User{id: invited_id} = user_fixture()
 
-    Friendship.send_invite(%{invitee_id: invitee_id, invited_id: invited_id})
+    {:ok, invite} = Friendship.send_invite(%{invitee_id: invitee_id, invited_id: invited_id})
+    Repo.preload(invite, :invitee)
+  end
+
+  def invite_fixture(invited_id) do
+    %User{id: invitee_id} = user_fixture()
+    {:ok, invite} = Friendship.send_invite(%{invitee_id: invitee_id, invited_id: invited_id})
+    Repo.preload(invite, :invitee)
   end
 
   def two_day_invite() do
