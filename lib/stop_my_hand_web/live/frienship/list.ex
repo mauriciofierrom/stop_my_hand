ebc4449797179fe1556afb173eab89226cd3a1e7
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-01-07 11:00:32 -0500

    Add presence tracking to List LiveView
    - Add tracking on friends loading via handle_async callback
    - Handle incoming leave/join events from Presence
    - Update both the friendlist to show the status in the UI and the Cache
      to have the information ready when needed for future features
    - Add a visual indicator for users being online/offline

--- lib/stop_my_hand_web/live/frienship/list.ex
+++ lib/stop_my_hand_web/live/frienship/list.ex
@@ -5,6 +5,8 @@ defmodule StopMyHandWeb.Friendship.List do
   alias Phoenix.LiveView.AsyncResult
   alias StopMyHandWeb.Dropdown
   alias StopMyHandWeb.Endpoint
+  alias StopMyHandWeb.Presence
+  alias StopMyHand.Cache
 
   def render(assigns) do
     ~H"""
@@ -27,7 +29,7 @@ defmodule StopMyHandWeb.Friendship.List do
       <:failed :let={_failure}>there was an error fetching invites</:failed>
       <div class={[]}>
         <%= if !Enum.empty?(friends) do %>
-          <%= for friend <- friends do %>
+          <%= for {_n, friend} <- friends do %>
             <.friend_item friend={friend}/>
           <% end %>
         <% else %>
@@ -41,11 +43,27 @@ defmodule StopMyHandWeb.Friendship.List do
   def mount(_params, _session, socket) do
     current_user = socket.assigns.current_user
 
-    Endpoint.subscribe("friendlist:#{current_user.id}")
-
     {:ok, socket
     |> assign_async(:invites, fn -> {:ok, %{invites: Friendship.get_pending_invites(current_user.id)}} end)
-    |> assign_async(:friends, fn -> {:ok, %{friends: Friendship.get_friends(current_user.id)}} end)}
+    |> assign(:friends, AsyncResult.ok([])) #}
+    |> start_async(:fetch_friends, fn -> Friendship.get_friends(current_user.id) end)}
+  end
+
+  def handle_async(:fetch_friends, {:ok, friends}, socket) do
+    current_user = socket.assigns.current_user
+
+    {fs, ls} = Enum.reduce(friends, {[], []}, fn friend, {fs, ls} ->
+      status = Presence.get_status(friend.id)
+      {Enum.into(%{friend.id => %{user: friend, status: status}}, fs), [{friend.id, status}|ls]}
+    end)
+
+    Cache.load_online_friend_list(%{user_id: current_user.id, list: ls})
+
+    Presence.track(socket.transport_pid, "online_users", current_user.id, %{})
+
+    Presence.subscribe_friends_updates(current_user.id)
+
+    {:noreply, assign(socket, :friends, AsyncResult.ok(fs))}
   end
 
   def handle_event("accept_invite", %{"inviteid" => inviteid}, socket) do
@@ -103,6 +121,26 @@ defmodule StopMyHandWeb.Friendship.List do
     |> put_flash(:info, "Invitation received")}
   end
 
+  def handle_info(%{event: "join", payload: {_, {_, user_id}}}, socket) do
+    current_user = socket.assigns.current_user
+
+    %AsyncResult{result: friends} = socket.assigns.friends
+
+    new_friends = handle_presence(current_user, friends, user_id, :online)
+
+    {:noreply, assign(socket, :friends, AsyncResult.ok(new_friends))}
+  end
+
+  def handle_info(%{event: "leave", payload: {_, {_, user_id}}}, socket) do
+    current_user = socket.assigns.current_user
+
+    %AsyncResult{result: friends} = socket.assigns.friends
+
+    new_friends = handle_presence(current_user, friends, user_id, :offline)
+
+    {:noreply, assign(socket, :friends, AsyncResult.ok(new_friends))}
+  end
+
   defp invite_item(assigns) do
     ~H"""
     <div
@@ -122,17 +160,43 @@ defmodule StopMyHandWeb.Friendship.List do
         "flex flex-row"
     ]}>
       <div class="basis--2/3">
-        <%= assigns.friend.username %>
-        <.live_component module={Dropdown} id={assigns.friend.id}>
+        <%= assigns.friend.user.username %>
+        <span class={status_message_class(assigns.friend.status)}><%= assigns.friend.status %></span>
+        <.live_component module={Dropdown} id={assigns.friend.user.id}>
           <:button>
             ...
           </:button>
           <.dropdown_item>
-            <span class="text-red-800" id={ "remove-#{assigns.friend.id}"} phx-hook="ConfirmFriendRemoval" data-userid={ assigns.friend.id }>Delete</span>
+            <span class="text-red-800" id={ "remove-#{assigns.friend.user.id}"} phx-hook="ConfirmFriendRemoval" data-userid={ assigns.friend.user.id }>Delete</span>
           </.dropdown_item>
         </.live_component>
       </div>
     </div>
     """
+  end
+
+  defp status_message_class(status) do
+    base = ["text-sm italic"]
+    case status do
+      :online -> ["text-green-500" | base]
+      _ -> ["text-gray-500" | base]
+    end
+  end
+
+  defp handle_presence(current_user, friends, user_id, event) do
+    friend_ids = Enum.map(friends, fn {k, %{status: status}} ->
+      new_status = if k == user_id, do: event, else: status
+
+      {k, new_status} end)
+
+    Cache.load_online_friend_list(%{user_id: current_user.id, list: friend_ids})
+
+    Enum.map(friends, fn {k, %{user: friend, status: status}} ->
+      if user_id == friend.id do
+        {k, %{user: friend, status: event}}
+      else
+        {k, %{user: friend, status: status}}
+      end
+    end)
   end
 end
