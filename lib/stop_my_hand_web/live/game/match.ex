c01f10a59997f75bd06810b9c31f0f988dd094d8
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-02-03 23:43:52 -0500

    Start MatchDriver on player loading
    Once the async fetch of the players for the match is finished we launch
    the MatchDriver via the DynamicSupervisor and pick the first letter to
    dispatch the JS event that starts the JS channel client

--- lib/stop_my_hand_web/live/game/match.ex
+++ lib/stop_my_hand_web/live/game/match.ex
@@ -2,18 +2,24 @@ defmodule StopMyHandWeb.Game.Match do
   use StopMyHandWeb, :live_view
 
   alias StopMyHand.Game
+  alias StopMyHand.Game.Player
   alias StopMyHandWeb.Endpoint
+  alias StopMyHand.MatchDriver
 
   def render(assigns) do
     ~H"""
     <h1>Stop my hand!</h1>
     <div id="counter">3</div>
-    <div id="game" class="hidden">GAME IN PROGRESS</div>
+    <div id="game" class="hidden">
+      GAME IN PROGRESS
+      <div id="letter"></div>
+    </div>
     """
   end
 
   def mount(params, _session, socket) do
     match_id = params["match_id"]
+
     {:ok, socket
     |> assign(:match_id, match_id)
     |> start_async(:fetch_players, fn -> Game.get_match_players(match_id) end)
@@ -21,10 +27,20 @@ defmodule StopMyHandWeb.Game.Match do
   end
 
   def handle_async(:fetch_players, {:ok, players}, socket) do
-    player_ids = Enum.map(players, &(&1.id))
+    player_ids = Enum.map(players, fn %Player{user_id: user_id} -> user_id end)
+    match_id = socket.assigns.match_id
+    match = Game.get_match(match_id)
+
+    # Start the driver for this match
+    DynamicSupervisor
+      .start_child(StopMyHand.DynamicSupervisor,
+        {StopMyHand.MatchDriver,
+         %{player_ids: [match.creator_id | player_ids], match_id: match_id}})
+
+    {:ok, letter} = MatchDriver.pick_letter(match_id)
 
     {:noreply, push_event(socket, "connect_match", %{
-               match_id: socket.assigns.match_id,
+               match_id: match_id,
                timestamp: System.system_time(:millisecond)})}
   end
 end
