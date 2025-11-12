a30e284a7d992bc467f5c6c33c796ad1c142e4ce
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-11-12 11:29:17 -0500

    Update the home page
    Just a hero banner with a subtitle for now

--- lib/stop_my_hand_web/live/user_login_live.ex
+++ lib/stop_my_hand_web/live/user_login_live.ex
@@ -9,7 +9,7 @@ defmodule StopMyHandWeb.UserLoginLive do
         Sign in to account
         <:subtitle>
           Don't have an account?
-          <.link navigate={~p"/users/register"} class="font-semibold text-brand hover:underline">
+          <.link navigate={~p"/users/register"} class="font-semibold text-accent hover:underline">
             Sign up
           </.link>
           for an account now.
