1aa149262b8f08f7421fb6bd18613356e3327e7f
Mauricio Fierro <mauriciofierrom@gmail.com>
2025-11-28 10:28:16 -0500

    Add round start synchronization
    Game channel reports to Match Driver with players that have joined to
    drive the round start
    
    - On channel join tell the driver to update the list of joined vs the
      expected list it's been initalized with.
    - If minimum quorum is met we can start the game (for now)
    - If not enough quorum has been gathered before a timeout we stop the driver
    - The driver starts the match as soon as there's quorum based on a
    timeout (pending)

--- lib/stop_my_hand/match_driver.ex
+++ lib/stop_my_hand/match_driver.ex
@@ -4,16 +4,32 @@ defmodule StopMyHand.MatchDriver do
   """
   use GenServer
 
+  alias StopMyHandWeb.Endpoint
+
+  @quorum_timeout 15000
+  @match_topic "match"
+  @countdown 3
+
   def start_link(%{player_ids: player_ids, match_id: match_id}) do
-    GenServer.start_link(__MODULE__, player_ids, name: via_tuple(match_id))
+    GenServer.start_link(__MODULE__, {player_ids, match_id}, name: via_tuple(match_id))
   end
 
-  def init(expected_player_ids) do
+  def init({ expected_player_ids, match_id}) do
+    {starting_letter, alphabet} = pick_letter_from(make_alphabet())
+
+    Process.send_after(self(), :no_quorum, @quorum_timeout)
+
     initial_state = %{
+      match_id: match_id,
+      alphabet: alphabet,
+      round: 1,
+      letter: starting_letter,
       expected: expected_player_ids,
-      alphabet: make_alphabet(),
-      joined: []
+      joined: [],
+      pending: [],
+      game_status: :init,
     }
+
     {:ok, initial_state}
   end
 
@@ -25,33 +41,67 @@ defmodule StopMyHand.MatchDriver do
     GenServer.call(via_tuple(match_id), :pick_letter)
   end
 
-  def all_players_in?(match_id) do
-    GenServer.call(via_tuple(match_id), :check_all_players_in)
+  def handle_call({:add_player, player_id}, _from, %{expected: expected, joined: joined, game_status: :init} = state) when length(joined) == length(expected) do
+    # Set the status to started when there's quorum
+    new_status = if length(joined) >= 1, do: :ongoing, else: state.game_status
+
+    Process.send_after(self(), :game_start, 1000)
+
+    {:noreply, %{state|joined: [player_id|joined], game_status: :ongoing}}
   end
 
+  # Only on the init state we add players to mark them as JOINED
+  def handle_call({:add_player, player_id}, _from, %{joined: joined, game_status: :init} = state) when length(joined) >= 1 do
+    Process.send_after(self(), :game_start, 200)
 
-  def handle_call({:add_player, player_id}, _from, %{joined: joined} = state) do
-    {:reply, :ok, %{state|joined: [player_id|joined]}}
+    {:reply, %{ok: :pending}, %{state|joined: [player_id|joined], game_status: :ongoing}}
   end
+
+  # Only on the init state we add players to mark them as JOINED
+  def handle_call({:add_player, player_id}, _from, %{joined: joined, game_status: :init} = state) do
+    {:reply, %{ok: :pending}, %{state|joined: [player_id|joined]}}
+  end
+
+  # On any other game state the player is considered pending and will be moved before the new round starts
+  def handle_call({:add_player, player_id}, _from, %{pending: pending} = state) do
+    {:reply, :ok, %{state|pending: [player_id|pending]}}
+  end
+
+  def handle_call(:pick_letter, _from, %{alphabet: []} = state), do: {:reply, {:error, "No more letters"}, state}
 
   def handle_call(:pick_letter, _from, %{alphabet: alphabet} = state) do
-    letter = Enum.random(alphabet)
-    if Enum.empty?(alphabet) do
-      {:reply, {:error, "No more letters"}, state}
-    else
-      {:reply, {:ok, letter}, %{state|alphabet: List.delete(alphabet, letter)}}
-    end
+    {letter, new_alphabet} = pick_letter_from(alphabet)
+    {:reply, {:ok, letter}, %{state|alphabet: new_alphabet}}
   end
 
-  def handle_call(:check_all_players_in, _from, %{expected: expected, joined: joined} = state) do
-    {:reply, Enum.all?(joined, &(Enum.member?(expected, &1))), state}
+  def handle_info(:game_start, state) do
+    payload = %{
+      letter: state.letter,
+      round: state.round,
+      countdown: @countdown
+    }
+
+    Endpoint.broadcast!("#{@match_topic}:#{state.match_id}", "game_start", payload)
   end
 
-  defp via_tuple(match_id) do
-    {:via, Registry, {StopMyHand.Registry, match_id}}
+  def handle_info(:no_quorum, %{game_status: :init} = state) do
+    GenServer.stop(self(), :no_quorum, 5000)
+  end
+
+  def handle_info(:no_quorum, state) do
+    {:noreply, state}
   end
 
   defp make_alphabet() do
     Enum.map(65..90, &<<&1::utf8>>)
+  end
+
+  defp pick_letter_from(alphabet) do
+    letter = Enum.random(alphabet)
+    {letter, List.delete(alphabet, letter)}
+  end
+
+  defp via_tuple(match_id) do
+    {:via, Registry, {StopMyHand.Registry, match_id}}
   end
 end
