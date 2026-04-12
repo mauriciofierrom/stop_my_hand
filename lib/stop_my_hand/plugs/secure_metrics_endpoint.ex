350a6342c52ad5c784fa0f001def3e810ba250ad
Mauricio Fierro <mauriciofierrom@gmail.com>
2026-04-11 21:45:44 -0500

    Fix metrics header check
    Get the header from the result list and include `Bearer` in the check
    via interpolation.

--- lib/stop_my_hand/plugs/secure_metrics_endpoint.ex
+++ lib/stop_my_hand/plugs/secure_metrics_endpoint.ex
@@ -5,6 +5,6 @@ defmodule StopMyHand.SecureMetricsEndpoint do
   def call(conn, env_var) do
     auth_header = Plug.Conn.get_req_header(conn, "authorization")
 
-    System.get_env(env_var) == auth_header
+    List.first(auth_header) == "Bearer #{System.get_env(env_var)}"
   end
 end
